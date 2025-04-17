4eca74bbd2ce251a74d6f5ce1ed3313ffba43703
Mebin Thattil <mail@mebin.in>
2025-04-17 08:45:27 +0530

    Basic TUI integration complete
    This commit introduces a version of TUI integration which is fairly stable, and looks better.
    
    What changed:
    - Display logo
    - Created `LandingScreen()` and `InputBox()` functions. These display things that are shown when the program starts. Input box provides a spotlight search of sorts, where you can type any command and get it to do stuff.
    - Created `clearRow()` function that takes `tb` and `row` as parameters and clears the entire terminal buffer for that line
    - Created a way take a string input by combining multiple key presses, using a temporary string.
    - Updated the `POST` retreval, now it includes a message that says `Fetching posts...` and then removes it from buffer once the posts are fetched.
    
    TODO:
    - There is a case where if you enter too many characters it overflows out of the `input box`, need to fix that.
    - For character that input multiple character at once to the input box (E.G: `down`arrow on keyboard), glitches, cause the cursor pointer is set to increment by one, and does not accomodate this. So what happens is that `DOWN` is printed then if I say type the character `J` it prints `DJWN`. Simple fix to the cursor pointer increment should do.
    - Better documentation and man pages.

--- src/tui.nim
+++ src/tui.nim
@@ -11,11 +11,39 @@ illwillInit(fullscreen=true)
 setControlCHook(exitProc)
 hideCursor()
 
+var logo: string = """
+███╗░░██╗██╗███╗░░░███╗██████╗░██╗░░░██╗░██████╗
+████╗░██║██║████╗░████║██╔══██╗██║░░░██║██╔════╝
+██╔██╗██║██║██╔████╔██║██████╦╝██║░░░██║╚█████╗░
+██║╚████║██║██║╚██╔╝██║██╔══██╗██║░░░██║░╚═══██╗
+██║░╚███║██║██║░╚═╝░██║██████╦╝╚██████╔╝██████╔╝
+╚═╝░░╚══╝╚═╝╚═╝░░░░░╚═╝╚═════╝░░╚═════╝░╚═════╝░
+"""
+
 var tb = newTerminalBuffer(terminalWidth(), terminalHeight())
 
-tb.setForegroundColor(fgBlack, true)
-tb.drawRect(0, 0, terminalWidth(), terminalHeight())
-tb.drawHorizLine(2, 38, 6, doubleStyle=false)
+proc LandingScreen(): void = 
+  tb.setForegroundColor(fgBlack, true)
+  tb.drawRect(0, 0, terminalWidth(), terminalHeight())
+  tb.drawHorizLine(2, 50, 9, doubleStyle=true)
+
+proc InputBox(): void =
+  for column in 2..terminalWidth():
+      tb.write(column,19," ")
+
+  tb.drawRect(2,17,35,21)
+  tb.write(2,17," Spotlight Commands ")
+
+proc clearRow(tb: var TerminalBuffer, row: int, till_end : bool = false ): void =
+  var RowToClearTill : int = row
+  if till_end == true:
+    RowToClearTill = terminalHeight()-1
+  tb.display()
+
+  for row in row..RowToClearTill:
+    for column in 1..terminalWidth()-1:
+      tb.write(column, row, " ") 
+
 
 # need to get the user handle via input, will do 
 # TODO 
@@ -24,43 +52,62 @@ var client = initBlueskyClient()
 client.authenticate()
 let userHandle = "mebt.bsky.social"
 
+#below code splits the logo which is a multiline into single lines
+#then prints these one by one
+var SplitOfLogo: seq[string] = logo.split("\n")
+for i,line in SplitOfLogo:
+  tb.write(2,2+i, fgWhite, line)
 
-
-tb.write(2, 1, fgWhite, "Press the following to perform an action: ")
-tb.write(2, 2, fgWhite, "Type : ",fgGreen, "post", fgWhite, " to view all posts")
-tb.write(2, 5, "Press ", fgYellow, "ESC", fgWhite,
+tb.write(2, 11, fgWhite, "Press the following to perform an action: ")
+tb.write(2, 12, fgWhite, "Type : ",fgGreen, "post", fgWhite, " to view all posts")
+tb.write(2, 15, "Press ", fgYellow, "ESC", fgWhite,
                " or ", fgYellow, "Q", fgWhite, " to quit")
 
+
 # MAIN EVENT LOOOOOOOOP
+
 var i:int = 0
 var temp_str: string = ""
+LandingScreen()
+InputBox()
+
 while true:
   var key = getKey()
   case key
   of Key.None: discard
   of Key.Escape, Key.Q: exitProc()
-  #of Key.P:
-    #for i,post in posts:
-        #tb.write(2,8+i,resetStyle,$key)
+  of Key.Enter:
+          clearRow(tb,22, till_end = true) #clearing input box everytime
+          InputBox() #re-initilize input box, clearing previous contents
+          tb.write(4,19,temp_str)
+          case temp_str
+          of "POST":
+              tb.write(2,22,fgGreen,"Fetching posts...")
+              tb.display() #this is required the flush the buffer, else it doesn't seem to do it automatically
+
+              let posts = client.getAllPostsByUser(userHandle) #moved inside so that function gets called only when input is "POST"
+              clearRow(tb,22) #clear the 'fetching posts...' on line 22 first
+              tb.write(2,22,fgGreen,"Your posts:")
+              for i,post in posts:
+                  tb.write(2,25+i,resetStyle,post["text"].getStr())
+              
+              InputBox() #clearning row with temp typed string
+              temp_str = "" #clearing temp_string again cause entered pressed
+              i = 0 #resetting cursor position
+              continue
+          else:
+              clearRow(tb,25,till_end = true)
+              tb.write(2,23,"What man? I don't understand that command ")
+              temp_str = "" #clearing temp_string again cause entered pressed
+              i = 0 #resetting cursor position
+              InputBox()
+              continue
 
   else:
-    tb.write(2+i,8,$key)
+
+    tb.write(4+i,19,$key)
     i+=1
-    case key
-    of Key.Enter:
-        tb.write(2,10,temp_str)
-        case temp_str
-        of "POST":
-            let posts = client.getAllPostsByUser(userHandle) #moved inside so that function gets called only when input is "POST"
-            for i,post in posts:
-                tb.write(2,15+i,resetStyle,post["text"].getStr())
-        else:
-            tb.write(2,15,"HUH")
-            continue
-    else:
-        temp_str = temp_str & $key
-    #tb.write(2,10,$i)
-    tb.write(2, 7, resetStyle, "What does ", fgGreen, $key, resetStyle, " do man?!")
+    temp_str = temp_str & $key #append character to temp_string if key pressed is not enter
 
   tb.display()
   sleep(20)
