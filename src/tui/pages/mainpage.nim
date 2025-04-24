4a9f2c55a8f18af44af3d3cbd02785b953c0e595
Mebin Thattil <mail@mebin.in>
2025-04-24 14:55:27 +0530

    Added bsky.nim and dummy mainpage

--- src/tui/pages/mainpage.nim
+++ src/tui/pages/mainpage.nim
@@ -3,6 +3,8 @@ import illwill
 import ./../element
 import ./../helpers
 import ./../tui
+import ./../../bsky/bsky
+import json
 
 const nimbusLogo = """
 ███╗░░██╗ ██╗ ███╗░░░███╗ ██████╗░ ██╗░░░██╗ ░██████╗
@@ -16,9 +18,19 @@ const nimbusLogo = """
 proc mainPageSetup*(state: var TUIState) = 
   var center = findCenter(terminalWidth(), terminalHeight())
 
+  proc getPosts() =
+    var client = initBlueskyClient()
+    client.authenticate()
+    let userHandle = "mebin.in"
+    let posts = client.getAllPostsByUser(userHandle)
+    for post in posts:
+      state.elements.add(Element(Label(x: center.x - 19, y: center.y, fg: fgWhite, bg: bgNone, text: post.getStr())))
+
+
   state.elements = @[
     Element(Heading(x: center.x, y: center.y - 7, fg: fgGreen, bg: bgNone, text: nimbusLogo, centered: true)),
     Element(Label(x: center.x - 19, y: center.y, fg: fgWhite, bg: bgNone, text: "Welcome to mainpage:")),
+    Element(Button(x: center.x - 11, y: center.y + 6, fg: fgWhite, bg: bgBlack, inFocus: false, text: "Posts", width: 30, height: 1, action: proc() = getPosts()))
   ]
 
   state.findFocusableElements()
