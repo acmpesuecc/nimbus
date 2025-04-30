fc01505b2189b2cce813f2f674062c54feeca7d0
Mebin Thattil <mail@mebin.in>
2025-04-30 09:43:35 +0530

    Login button now writes to .env file with input taken via input box

--- src/tui/pages/login.nim
+++ src/tui/pages/login.nim
@@ -18,6 +18,8 @@ const nimbusLogo = """
 proc loginPageSetup*(state: var TUIState) = 
   var center = findCenter(terminalWidth(), terminalHeight())
 
+  
+
   proc mainpageRoute() =
     proc initTUIState(state: var TUIState) = #TODO: Somebody plj help, I'm not able to import this function from nimbus.nim, just put this here for now
       state.tb = newTerminalBuffer(terminalWidth(), terminalHeight())
@@ -44,14 +46,26 @@ proc loginPageSetup*(state: var TUIState) =
       if redraw:
         tuiState.update()
         tuiState.tb.display()
+    
+  proc writeToEnv(Handle, Password: InputField) =
+    let PDSHOST : string = "PDSHOST=https://bsky.social" & "\n"
+    let BLUESKY_HANDLE : string = "BLUESKY_HANDLE=" & Handle.text & "\n"
+    let APP_PASSWORD : string = "APP_PASSWORD=" & Password.text & "\n"
 
+    writeFile(".env", PDSHOST & BLUESKY_HANDLE & APP_PASSWORD)
+    writeFile("src/bsky/.env", PDSHOST & BLUESKY_HANDLE & APP_PASSWORD)
+
+    mainpageRoute()
+
+  var BLUESKY_HANDLE : InputField = InputField(x: center.x - 11, y: center.y, fg: fgWhite, bg: bgBlack, inFocus: false, width: 30, text: "", cursorPos: 0)
+  var APP_PASSWORD : InputField = InputField(x: center.x - 11, y: center.y + 3, fg: fgWhite, bg: bgBlack, inFocus: false, width: 30, text: "", cursorPos: 0)
   state.elements = @[
     Element(Heading(x: center.x, y: center.y - 7, fg: fgGreen, bg: bgNone, text: nimbusLogo, centered: true)),
     Element(Label(x: center.x - 19, y: center.y, fg: fgWhite, bg: bgNone, text: "Handle:")),
     Element(Label(x: center.x - 19, y: center.y + 3, fg: fgWhite, bg: bgNone, text: "Passwd:")),
-    Element(InputField(x: center.x - 11, y: center.y, fg: fgWhite, bg: bgBlack, inFocus: false, width: 30, text: "", cursorPos: 0)),
-    Element(InputField(x: center.x - 11, y: center.y + 3, fg: fgWhite, bg: bgBlack, inFocus: false, width: 30, text: "", cursorPos: 0)),
-    Element(Button(x: center.x - 11, y: center.y + 6, fg: fgWhite, bg: bgBlack, inFocus: false, text: "Login", width: 30, height: 1, action: proc() = mainpageRoute()))
+    Element(BLUESKY_HANDLE),
+    Element(APP_PASSWORD),
+    Element(Button(x: center.x - 11, y: center.y + 6, fg: fgWhite, bg: bgBlack, inFocus: false, text: "Login", width: 30, height: 1, action: proc() = writeToEnv(BLUESKY_HANDLE, APP_PASSWORD)))
   ]
 
   state.findFocusableElements()
